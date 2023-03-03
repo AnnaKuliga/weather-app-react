ec7ef8912885306b14e0d8a0fb7d53300ccc43aa
Anna <118673659+AnnaKuliga@users.noreply.github.com>
2023-03-03 13:13:01 +0100

    Added a weather component

---
+++ src/Weather.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Footer from "./Footer";
+
+export default function Weather() {
+  return (
+    <div>
+      <header>
+        <h1> Weather App</h1>
+      </header>
+      <main>
+        <h2> Weather forecast</h2>
+      </main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  );
+}
