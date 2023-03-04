0bf69034c04491f97134d245421d9e33268f2786
Anna <118673659+AnnaKuliga@users.noreply.github.com>
2023-03-04 10:12:35 +0100

    Added Time Zone component

--- src/Weather.js
+++ src/Weather.js
@@ -1,12 +1,21 @@
 import React from "react";
 import Footer from "./Footer";
 import SearchEngine from "./SearchEngine";
+import "./Weather.css";
 
 export default function Weather() {
   return (
     <div className="WeatherBody">
       <header>
-        <h1> Weather Forecast</h1>
+        <div className="TimeZone">
+          <h1> Time Zones</h1>
+          <ul className="navigation grid grid-four-columns">
+            <li className="navigation-item">London</li>
+            <li className="navigation-item">New York</li>
+            <li className="navigation-item">Sydney</li>
+            <li className="navigation-item">Tokyo</li>
+          </ul>
+        </div>
       </header>
       <main>
         <SearchEngine />
